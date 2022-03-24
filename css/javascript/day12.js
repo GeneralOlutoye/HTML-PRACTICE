572cd31a5dc350e8964b1afd05dbbfd4d890c5c1
GeneralOlutoye <donronald27@gmail.com>
2022-03-24 14:50:13 +0100

    Created javascript

---
+++ css/javascript/day12.js
@@ -0,0 +1,44 @@
+var greeting = "Hello, World";
+console.log(greeting);
+
+var sail = "Developer's cabal";
+console.log(sail);
+
+var status = "Watching Netflix";
+Mystatus = "Relaxing at the beach";
+console.log(Mystatus);
+
+mysail = "General";
+console.log(mysail);
+
+var myDressing = "so modest";
+mydressing = "so highclass";
+console.log(mydressing);
+
+myDressing = mysail;
+console.log(myDressing);
+
+var campus = "senate building";
+var church = "mission house";
+
+church = campus;
+console.log(church);
+
+Mychurch = "redeem";
+console.log(Mychurch);
+
+var temperature = "100 degrees celcius";
+temperature = "0 degrees celcius";
+console.log(temperature);
+
+var numberOfLikes = 12;
+numberOfLikes = 13;
+console.log(numberOfLikes);
+
+var instagramLike = 250;
+console.log(instagramLike+10);
+
+var myCalculator = 3;
+myCalculator = 4;
+console.log(myCalculator/2);
+
