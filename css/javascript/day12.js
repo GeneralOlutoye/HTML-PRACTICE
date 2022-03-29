9648f8599273fc8e027442abbb98b86aa7dcd79c
GeneralOlutoye <donronald27@gmail.com>
2022-03-29 15:46:25 +0200

    Update day12.js

--- css/javascript/day12.js
+++ css/javascript/day12.js
@@ -151,9 +151,9 @@ console.log(daudu);
     {name:"Computer", price: 1000},
     {name:"Keyboard", price: 25},
 ];
-console.log(items.filter((items) =>{return items.price<=100}))
+// console.log(items.filter((items) =>{return items.price<=100}))
 
-// var newitems = items.filter((e)=>{return e.price<=100})
-// console.log(newitems)
+var newitems = items.filter((items)=>{return items.price<=100})
+console.log(newitems)
 
 
