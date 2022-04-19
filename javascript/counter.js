18c4d851aece9bd7ecd94c601c8124d72c148fb1
GeneralOlutoye <donronald27@gmail.com>
2022-04-19 12:37:29 +0200

    Updated mainGit

---
+++ javascript/counter.js
@@ -0,0 +1,15 @@
+let count=0
+document.getElementById("decrease").onclick= function(){
+    if(count<=0){document.getElementById(`counter`).innerHTML=Display(0)}
+    count -=1
+    document.getElementById(`counter`).innerHTML=count
+}
+document.getElementById("increase").onclick=function(){
+    count +=1
+document.getElementById(`counter`).innerHTML=count
+}
+document.getElementById("reset").onclick=function(){
+    count =0
+document.getElementById(`counter`).innerHTML=count
+}
+
