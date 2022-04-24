35787565c73749953387b4ac21de979d206d1d18
GeneralOlutoye <donronald27@gmail.com>
2022-04-24 13:06:19 +0100

    Updated calculator.js

--- javascript/calculator1.js
+++ javascript/calculator1.js
@@ -1,20 +1,5 @@
 let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
-//  function val(val) {
-//     document.getElementById("display").value += val
-// }
-// function equalsign() {
-//     var a = document.getElementById("display").value
-//     var b = eval(a)
-//     document.getElementById("display").value = b
-//     document.getElementById("display2").value+=a
-// }
-// function del(){
-//     display.value=display.value.slice(0, -1)
-// }
-// function AC(){
-//     display.value=""
-// }
 buttons.map( button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText){
