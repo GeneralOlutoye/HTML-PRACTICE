803b35d19ffd25db0f572b9eca5e554cc85a0a24
GeneralOlutoye <donronald27@gmail.com>
2022-04-28 13:40:13 +0100

    Created 28April folder

---
+++ 28April/28April.js
@@ -0,0 +1,25 @@
+let newtag = document.createElement('li')
+let myOwnList = document.getElementById('myList')
+newtag.textContent = 'peach'
+myOwnList.style.backgroundColor='skyblue'
+myOwnList.insertBefore(newtag, myOwnList.getElementsByTagName('li')[1])
+
+document.body.style.backgroundColor='gray'
+
+let newTag = document.createElement('h1')
+newTag.textContent='DOM'
+newTag.style.color = 'black'
+newTag.style.backgroundColor='violet'
+newTag.style.fontSize='30px'
+document.body.append(newTag)
+
+const show=document.getElementById('submit')
+show.onclick= ()=> alert('YOU WON')
+
+const showed= document.getElementById('change')
+showed.onchange= ()=> alert('you are doing great')
+
+const box= document.getElementById('move')
+box.onmouseover=() => box.style.backgroundColor='black'
+box.onmouseout=() => box.style.backgroundColor='green'
+
