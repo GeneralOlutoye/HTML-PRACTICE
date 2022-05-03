83af7481e2141f625b514a3410aa618d4826988c
GeneralOlutoye <donronald27@gmail.com>
2022-05-03 17:17:52 +0100

    Created Tic Tac Toe folder

---
+++ classwork/Tic tac toe/XandO.js
@@ -0,0 +1,13 @@
+let bx1, bx2, bx3, bx4, bx5, bx6, bx7, bx8, bx9; 
+
+bx1 = document.getElementById('row1A').value
+bx2 = document.getElementById('row1B').value
+bx3 = document.getElementById('row1C').value
+bx4 = document.getElementById('row2A').value
+bx5 = document.getElementById('row2B').value
+bx6 = document.getElementById('row2C').value
+bx7 = document.getElementById('row3A').value
+bx8 = document.getElementById('row3B').value
+bx9 = document.getElementById('row3C').value
+
+if((bx1 == 'X' ||  bx1 == 'O') && (bx2 == 'X' || bx2 == 'O'))
