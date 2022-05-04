8b27842cec4e67f8607b2eb78e90ab8598388ee2
GeneralOlutoye <donronald27@gmail.com>
2022-05-04 04:41:37 +0100

    Updated XandO

--- classwork/Tic tac toe/XandO.js
+++ classwork/Tic tac toe/XandO.js
@@ -1,3 +1,4 @@
+let display = document.getElementById('display');
 let bx1, bx2, bx3, bx4, bx5, bx6, bx7, bx8, bx9; 
 
 bx1 = document.getElementById('row1A').value
@@ -10,4 +11,5 @@ bx7 = document.getElementById('row3A').value
 bx8 = document.getElementById('row3B').value
 bx9 = document.getElementById('row3C').value
 
-if((bx1 == 'X' ||  bx1 == 'O') && (bx2 == 'X' || bx2 == 'O'))
+if((bx1 == 'x' ||  bx1 == 'X') && (bx2 == 'x' || bx2 == 'X')
+    && (bx3=='x' || bx3=='X'))
