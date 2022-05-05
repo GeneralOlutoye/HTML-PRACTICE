dfacd162ed3cc11674174b2090cf6d7c8e1e1dd7
GeneralOlutoye <donronald27@gmail.com>
2022-05-05 14:12:05 +0100

    Updated TicTac

---
+++ Bola Project/EMS.js
@@ -0,0 +1,6 @@
+let bar=document.querySelector('.fa');
+let links=document.querySelector('.links');
+bar.addEventListener('click', ()=>{
+    links.classList.toggle('show')
+    console.log('hello');
+})
