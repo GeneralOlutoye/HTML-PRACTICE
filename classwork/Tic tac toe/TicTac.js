9b554b4d99ae40971b612cd6aa68ab073026d82a
GeneralOlutoye <donronald27@gmail.com>
2022-05-09 15:33:13 +0100

    Updated TicTac

--- classwork/Tic tac toe/TicTac.js
+++ classwork/Tic tac toe/TicTac.js
@@ -34,14 +34,45 @@ function cellClicked(){
     updateCell(this, cellIndex)
 }
 function changePlayer(){
-
+    currentPlayer= (currentPlayer=='X')? 'O':'X'
+    statusText.textContent=`${currentPlayer}'s turn`
 }
-function updateCell(){
-
+function updateCell(cell, index){
+    options[index]= currentPlayer
+    cell.textContent= currentPlayer
 }
 function checkWinner(){
+    let roundWon = false
+    for (i=0; i<winConditions.length; i++){
+        const condition = winConditions[i]
 
-}
+        const cellA = options[condition[0]]
+        const cellB = options[condition[1]]
+        const cellC = options[condition[2]]
+
+        if(cellA=='' || cellB=='' || cellC==''){
+            continue
+        }
+        else if(cellA == cellB == cellC){
+            roundWon= true
+            break
+        }
+    }
+    if(roundWon){
+        statusText.textContent= `${currentPlayer}wins`
+        running= false
+    }
+    else if(!options.includes("")){
+        statusText.textContent= `its a draw`
+        running=false
+    }
+    else{
+        changePlayer()
+    }
+} 
 function reset(){
-
+    currentPlayer=`X`
+    statusText.textContent= `${currentPlayer}'s turn`
+    cells.forEach(cell=>cell.textContent='')
+    running=true 
 }
