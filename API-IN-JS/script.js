c62010044ac0235c9d6f1a13fad351b497ead659
GeneralOlutoye <donronald27@gmail.com>
2022-06-10 16:15:31 +0100

    Updated API

--- API-IN-JS/script.js
+++ API-IN-JS/script.js
@@ -1,39 +1,24 @@
-
-
-// const getData= ()=>{
-//     fetch('https://reqres.in/api/users?page=2').then(i=>{
-//         i.json();
-//     })
-//     .then(iData =>{
-//         console.log(iData)
-//     })
-// }
-
-// // getBtn.addEventListener('click', getData)
 const getBtn= document.querySelector('#getReq')
 const TxtArea= document.querySelector('#txtArea')
 const display= document.querySelector('#display')
 
 let city
-// const getData= ()=>{
-//     TxtArea.textContent = city
-//         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5d18828185d71c238c11c35c2d12b7eb&units=metric`)
-//         .then(i=>{
-//             i.json();
-//         })
-//             .then(iData =>{
-//                 display.textContent=iData.main.temp
-//             })
-//     }
-
 getBtn.addEventListener('click', () => {
     city = TxtArea.value 
     fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5d18828185d71c238c11c35c2d12b7eb&units=metric`)
-    .then (joel =>{
-        return joel.json()
+    .then (weather =>{
+        return weather.json()
     })
     .then(olutoye =>{
-        display.textContent=`${olutoye.main.temp}c`
+        display.textContent=`The weather in ${city} is ${olutoye.main.temp}°C`
     })
 })
 
+let course= 'softwareDev' 
+let price= 100
+let main_goal=['work as a dev', 'make lots of money', 'build my own app']
+
+alert(goal)
+console.log(course, price, main_goal)
+
+console.log(main_goal[1])
