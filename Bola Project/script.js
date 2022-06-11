1e59576e28b5ef50a4f7df09eb8d3231a59a0be3
GeneralOlutoye <donronald27@gmail.com>
2022-06-11 11:04:01 +0100

    Moved Folders from HTML-Practice

--- Bola Project/script.js
+++
@@ -1,6 +0,0 @@
-let bar=document.querySelector('.fa');
-let links=document.querySelector('.links');
-bar.addEventListener('click', ()=>{
-    links.classList.toggle('show')
-    console.log('hello');
-})
--- RockPaperScissors/script.js
+++
@@ -1,1 +0,0 @@
-let add= document.getElementById('add')
